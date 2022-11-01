72043039beaff1fa5a6346d7e66e8d13e961c330
Avishek <avishek.biswas@digitas.com>
2022-11-01 10:53:32 +0530

    registration page form validation done

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,13 +2,20 @@
  * @format
  */
 
-import 'react-native';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow, configure} from 'enzyme';
 import React from 'react';
-import App from '../App';
+import App from '../App.tsx';
 
-describe('App', () => {
-  it('should render successfully', () => {
-    const { container } = render(<App />);
-    expect(container).toBeTruthy();
+// Note: test renderer must be required after react-native.
+//import renderer from 'react-test-renderer';
+
+configure({adapter: new Adapter()});
+
+describe('App Test1', () => {
+  test('test caseApp 1', () => {
+    const componentone = shallow(<App />);
+    console.log(componentone.debug(), 'RESTLLLT');
+    expect(componentone).toMatchSnapshot();
   });
 });
--- src/screens/Registration.tsx
+++ src/screens/Registration.tsx
@@ -11,23 +11,46 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
   View,
+  Button,
+  TextInput,
 } from 'react-native';
-
+import {RadioButton} from 'react-native-paper';
+import {Dropdown} from 'react-native-element-dropdown';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import SizedBox from '../components/Sizebox';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import Feather from 'react-native-vector-icons/Feather';
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
-import {TextInput} from 'react-native-paper';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+
 import {auth} from '../../firebase';
 import {createUserWithEmailAndPassword} from 'firebase/auth';
 
 const Registration = ({navigation}: any) => {
-  const [name, setName] = useState<String>('');
+  const data = [
+    {label: 'Post Graduate', value: '1'},
+    {label: 'Graduate', value: '2'},
+    {label: 'HSC/Diploma', value: '3'},
+    {label: 'SSC', value: '4'},
+  ];
+  const [fname, setFName] = useState<String>('');
+  const [lname, setLName] = useState<String>('');
+  const [phone, setPhone] = useState<String>('');
   const [email, setEmail] = useState<String>('');
   const [pass, setPass] = useState<String>('');
+  const [checked, setChecked] = useState<String>('Male');
   const [confirmpass, setConfirmPass] = useState<String>('');
-  const [nameValid, setNameValid] = useState<Boolean>(false);
+  const [fnameValid, setFNameValid] = useState<Boolean>(false);
+  const [lnameValid, setLNameValid] = useState<Boolean>(false);
+  const [phoneValid, setPhoneValid] = useState<Boolean>(false);
   const [emailValid, setEmailValid] = useState<Boolean>(false);
   const [passValid, setPassValid] = useState<Boolean>(false);
   const [confirmValid, setConfirmValid] = useState<Boolean>(false);
+  const [value, setValue] = useState<String>('');
+  const [show, setShow] = useState(false);
+  const [text, setText] = useState<String>('');
 
   function checkPasswordValidity(value: string): any {
     const isNonWhiteSpace = /^\S*$/;
@@ -58,12 +81,29 @@ const Registration = ({navigation}: any) => {
     return null;
   }
 
-  const handleName = (value: any) => {
-    setName(value);
-    if (value.length === 0) {
-      setNameValid(true);
+  const handleFName = (value: any) => {
+    setFName(value);
+    if (value.length === 0 && value.length <= 3) {
+      setFNameValid(true);
     } else {
-      setNameValid(false);
+      setFNameValid(false);
+    }
+  };
+  const handleLName = (value: any) => {
+    setLName(value);
+    if (value.length === 0 && value.length <= 3) {
+      setLNameValid(true);
+    } else {
+      setLNameValid(false);
+    }
+  };
+
+  const handleNumber = (value: any) => {
+    setPhone(value);
+    if (value.length === 0 && value.length <= 3) {
+      setPhoneValid(true);
+    } else {
+      setPhoneValid(false);
     }
   };
 
@@ -101,6 +141,22 @@ const Registration = ({navigation}: any) => {
     }
   };
 
+  const showDatePicker = () => {
+    setShow(true);
+  };
+
+  const hideDatePicker = () => {
+    setShow(false);
+  };
+
+  const handleConfirm = date => {
+    let dateTimeString =
+      date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
+    setText(dateTimeString);
+
+    hideDatePicker();
+  };
+
   const handleSignUp: any = () => {
     createUserWithEmailAndPassword(auth, email, pass)
       .then(userCredentials => {
@@ -116,14 +172,38 @@ const Registration = ({navigation}: any) => {
 
   const onSubmit = () => {
     const checkPassowrd = checkPasswordValidity(pass);
-    if (name.length === 0) {
-      Alert.alert('Name can not be empty');
+    let current = new Date();
+    let todaysDate =
+      current.getDate() +
+      '-' +
+      (current.getMonth() + 1) +
+      '-' +
+      current.getFullYear();
+
+    if (fname.length === 0) {
+      Alert.alert('First name can not be empty');
+    } else if (fname.length < 3) {
+      Alert.alert('First name must contain 3 characters');
+    } else if (lname.length === 0) {
+      Alert.alert('Last name can not be empty');
+    } else if (lname.length < 3) {
+      Alert.alert('Last name must contain 3 characters');
+    } else if (phone.length === 0) {
+      Alert.alert('Phone number must contain 10 digits');
+    } else if (phone.length < 10) {
+      Alert.alert('Phone number must contain 10 digits');
     } else if (email.length === 0) {
       Alert.alert('Email can not be empty');
     } else if (pass.length === 0) {
       Alert.alert('Password should be min 8 characters and max 20 characters');
-    } else if (pass !== confirmpass) {
+    } else if (confirmpass !== pass) {
       Alert.alert('Password and confirm password should be same');
+    } else if (value.length === 0) {
+      Alert.alert('You must choose Education');
+    } else if (text.length === 0) {
+      Alert.alert('Date of birth can not be empty');
+    } else if (text === todaysDate) {
+      Alert.alert('Date of birth should be less than current date');
     } else if (!checkPassowrd) {
       handleSignUp();
     } else {
@@ -133,79 +213,232 @@ const Registration = ({navigation}: any) => {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.root}>
-        <SafeAreaView style={styles.safeAreaView}>
-          <KeyboardAvoidingView
-            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            style={styles.content}>
-            <View>
-              <TextInput
-                style={{marginTop: 10}}
-                label="Name"
-                value={name}
-                autoCapitalize="none"
-                mode="outlined"
-                onChangeText={handleName}
-              />
-              {nameValid ? (
-                <Text style={styles.textFailed}>Name can't be empty</Text>
-              ) : (
-                <Text style={styles.textFailed}> </Text>
-              )}
-
-              <TextInput
-                style={{marginTop: 10}}
-                label="Email"
-                value={email}
-                autoCapitalize="none"
-                mode="outlined"
-                onChangeText={handleCheckEmail}
-              />
-              {emailValid ? (
-                <Text style={styles.textFailed}>Wrong format email</Text>
-              ) : (
-                <Text style={styles.textFailed}> </Text>
-              )}
-              <TextInput
-                style={{marginTop: 10}}
-                label="Password"
-                value={pass}
-                mode="outlined"
-                onChangeText={handlePass}
-              />
-              {passValid ? (
-                <Text style={styles.textFailed}>
-                  Password can't be too short
+      <KeyboardAwareScrollView style={{backgroundColor: 'orange'}}>
+        <View style={styles.root}>
+          <SafeAreaView style={styles.safeAreaView}>
+            <KeyboardAvoidingView
+              behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+              style={styles.content}>
+              <View>
+                <Text style={{marginLeft: 10, fontWeight: 'bold'}}>
+                  FirstName*
                 </Text>
-              ) : (
-                <Text style={styles.textFailed}> </Text>
-              )}
+                <View style={styles.searchSection}>
+                  <Fontisto
+                    name="person"
+                    style={{paddingTop: 3, paddingLeft: 5}}
+                  />
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Enter your First Name here"
+                    value={fname}
+                    keyboardType="default"
+                    onChangeText={handleFName}
+                    underlineColor="transparent"
+                  />
+                </View>
+                {fnameValid ? (
+                  <Text style={styles.textFailed}>
+                    First name can't be empty
+                  </Text>
+                ) : (
+                  <Text style={styles.textFailed}> </Text>
+                )}
 
-              <TextInput
-                style={{marginTop: 10}}
-                label="Confirm Password"
-                value={confirmpass}
-                mode="outlined"
-                onChangeText={handleConfirmPass}
-              />
-              {confirmValid ? (
-                <Text style={styles.textFailed}>
-                  Confirm password can't be matched
+                <Text style={{marginLeft: 10, fontWeight: 'bold'}}>
+                  LastName*
                 </Text>
-              ) : (
-                <Text style={styles.textFailed} />
-              )}
-            </View>
+                <View style={styles.searchSection}>
+                  <Fontisto
+                    name="person"
+                    style={{paddingTop: 3, paddingLeft: 5}}
+                  />
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Enter your Last Name here"
+                    value={lname}
+                    keyboardType="default"
+                    onChangeText={handleLName}
+                    underlineColor="transparent"
+                  />
+                </View>
+                {lnameValid ? (
+                  <Text style={styles.textFailed}>
+                    Last Name can't be empty
+                  </Text>
+                ) : (
+                  <Text style={styles.textFailed}> </Text>
+                )}
 
-            <SizedBox height={20} />
-            <TouchableOpacity onPress={onSubmit}>
-              <View style={styles.button}>
-                <Text style={styles.buttonTitle}>Register</Text>
+                <Text style={{marginLeft: 10, fontWeight: 'bold'}}>
+                  Phone Number*
+                </Text>
+                <View style={styles.searchSection}>
+                  <Feather
+                    name="phone"
+                    style={{paddingTop: 3, paddingLeft: 5}}
+                  />
+                  <TextInput
+                    style={styles.input}
+                    keyboardType="phone-pad"
+                    maxLength={10}
+                    placeholder="Enter your 10 digit mobile number"
+                    value={phone}
+                    onChangeText={handleNumber}
+                    underlineColor="transparent"
+                  />
+                </View>
+                {phoneValid ? (
+                  <Text style={styles.textFailed}>
+                    Phone number must contain 10 digits
+                  </Text>
+                ) : (
+                  <Text style={styles.textFailed}> </Text>
+                )}
+
+                <Text style={{marginLeft: 10, fontWeight: 'bold'}}>Email*</Text>
+                <View style={styles.searchSection}>
+                  <Entypo
+                    name="message"
+                    style={{paddingTop: 3, paddingLeft: 5}}
+                  />
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    value={email}
+                    keyboardType="default"
+                    autoCapitalize="none"
+                    onChangeText={handleCheckEmail}
+                  />
+                </View>
+
+                {emailValid ? (
+                  <Text style={styles.textFailed}>Wrong format email</Text>
+                ) : (
+                  <Text style={styles.textFailed}> </Text>
+                )}
+                <Text style={{marginLeft: 10, fontWeight: 'bold'}}>
+                  Gender*
+                </Text>
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    marginTop: 5,
+                  }}>
+                  <Text style={styles.gender}>Male</Text>
+                  <RadioButton
+                    value="Male"
+                    status={checked === 'Male' ? 'checked' : 'unchecked'}
+                    onPress={() => setChecked('Male')}
+                  />
+                  <Text style={styles.gender}>Female</Text>
+                  <RadioButton
+                    value="Female"
+                    status={checked === 'Female' ? 'checked' : 'unchecked'}
+                    onPress={() => setChecked('Female')}
+                  />
+                </View>
+
+                <Text style={{marginLeft: 10, fontWeight: 'bold'}}>
+                  Password*
+                </Text>
+
+                <View style={styles.searchSection}>
+                  <Entypo name="lock" style={{paddingTop: 3, paddingLeft: 5}} />
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    value={pass}
+                    onChangeText={handlePass}
+                  />
+                </View>
+
+                {passValid ? (
+                  <Text style={styles.textFailed}>
+                    Password can't be too short
+                  </Text>
+                ) : (
+                  <Text style={styles.textFailed}> </Text>
+                )}
+
+                <Text style={{marginLeft: 10, fontWeight: 'bold'}}>
+                  Confirm Password*
+                </Text>
+                <View style={styles.searchSection}>
+                  <Entypo name="lock" style={{paddingTop: 3, paddingLeft: 5}} />
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Confirm Password"
+                    value={confirmpass}
+                    onChangeText={handleConfirmPass}
+                  />
+                </View>
+
+                {confirmValid ? (
+                  <Text style={styles.textFailed}>
+                    Confirm password can't be matched
+                  </Text>
+                ) : (
+                  <Text style={styles.textFailed} />
+                )}
+                <Text style={{marginLeft: 10, fontWeight: 'bold'}}>
+                  Education*
+                </Text>
+
+                <Dropdown
+                  style={styles.dropdown}
+                  placeholderStyle={styles.placeholderStyle}
+                  selectedTextStyle={styles.selectedTextStyle}
+                  inputSearchStyle={styles.inputSearchStyle}
+                  iconStyle={styles.iconStyle}
+                  data={data}
+                  maxHeight={300}
+                  labelField="label"
+                  valueField="value"
+                  placeholder="Select item"
+                  value={value}
+                  onChange={item => {
+                    setValue(item.value);
+                  }}
+                  renderLeftIcon={() => (
+                    <AntDesign
+                      style={styles.icon}
+                      color="black"
+                      name="Safety"
+                      size={20}
+                    />
+                  )}
+                />
+                <Text
+                  style={{marginLeft: 10, fontWeight: 'bold', marginTop: 10}}>
+                  Date Of Birth*
+                </Text>
+
+                <TouchableOpacity onPress={showDatePicker}>
+                  <View style={styles.searchSection}>
+                    <Text style={styles.input}>{text}</Text>
+                    <DateTimePickerModal
+                      isVisible={show}
+                      mode="date"
+                      onConfirm={handleConfirm}
+                      onCancel={hideDatePicker}
+                    />
+                  </View>
+                </TouchableOpacity>
               </View>
-            </TouchableOpacity>
-          </KeyboardAvoidingView>
-        </SafeAreaView>
-      </View>
+
+              <SizedBox height={20} />
+              <TouchableOpacity onPress={onSubmit}>
+                <View style={styles.button}>
+                  <Text style={styles.buttonTitle}>Submit</Text>
+                </View>
+              </TouchableOpacity>
+            </KeyboardAvoidingView>
+          </SafeAreaView>
+        </View>
+      </KeyboardAwareScrollView>
     </TouchableWithoutFeedback>
   );
 };
@@ -251,7 +484,6 @@ const styles = StyleSheet.create({
     width: 80,
   },
   root: {
-    backgroundColor: 'orange',
     flex: 1,
   },
   safeAreaView: {
@@ -286,5 +518,54 @@ const styles = StyleSheet.create({
   textFailed: {
     alignSelf: 'flex-end',
     color: 'red',
+  },
+  input: {
+    padding: 10,
+    flex: 1,
+  },
+  searchSection: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 40,
+    margin: 10,
+    borderWidth: 1,
+  },
+  gender: {
+    fontSize: 15,
+    color: 'black',
+    paddingLeft: 10,
+  },
+  dropdown: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 10,
+  },
+  icon: {
+    marginRight: 5,
+    marginLeft: 5,
+  },
+
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+  datePickerStyle: {
+    width: 200,
+    marginTop: 20,
   },
 });
